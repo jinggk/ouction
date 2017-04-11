18766781aa509a291bf6344e387f6250dcfce640
jingguokai <jinguokai@youbangsoft.com>
2017-04-11 15:46:25 +0800

    初步构建所有的组件

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode(); //判断是否要关闭angular的开发者模式
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
